e96714145358c51deb230fbd12f9f8c9c84be583
andrey <>
2020-02-15 20:09:03 +0300

    Версия ОС (выполнение 1го задания)

--- lesson_001/01_os_info.py
+++ lesson_001/01_os_info.py
@@ -2,8 +2,6 @@
 
 # Нужно собрать информацию об операционной системе и версии пайтона
 
-# TODO запустить этот скрипт и закомитить результат его работы (файл os_info.txt)
-
 import platform
 import sys
 
@@ -13,4 +11,3 @@ print(info)
 
 with open('os_info.txt', 'w', encoding='utf8') as ff:
     ff.write(info)
-
