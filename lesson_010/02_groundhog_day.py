c24ede02cb3d6c1626cff2cfe30cd906a87d7837
andrey <>
2020-05-06 20:39:47 +0300

    10 модуль 2 задание

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from random import randint
 
 # День сурка
 #
@@ -16,9 +17,31 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+exception_list = ['SuicideError', 'DepressionError', 'GluttonyError', 'CarCrashError', 'DrunkError', 'IamGodError']
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+
+def one_day():
+    dice = randint(1, 13)
+    if dice == 13:
+        error = exception_list[randint(0, 5)]
+        try:
+            if error in exception_list:
+                raise BaseException('исключение')
+        except BaseException as first_error:
+            print(f'Суть ошибки: {first_error}, {error}')
+
+
+    else:
+        global carma
+        carma = randint(1, 7)
+
+
+while ENLIGHTENMENT_CARMA_LEVEL >= 0:
+    one_day()
+    ENLIGHTENMENT_CARMA_LEVEL = ENLIGHTENMENT_CARMA_LEVEL - carma
+    if ENLIGHTENMENT_CARMA_LEVEL <= 0:
+        print('карма накоплена')
 
 # https://goo.gl/JnsDqu
