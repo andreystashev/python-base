9f7410b3705b4d25f5fe4230cad24c342ed8f996
andrey <>
2020-05-15 23:55:26 +0300

    9 модуль 3 задание

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,80 +22,49 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO Нейминг классов не по PEP8
-class nameError(Exception):
+class NamesError(Exception):
     pass
 
 
-class mailError(Exception):
+class MailError(Exception):
     pass
 
 
-class ageError(Exception):
+class AgeError(Exception):
     pass
 
 
-file_name = 'registrations.txt'
-file = open(file_name, mode='r')
+# TODO Не пойму, как разделить на 2 функции правильно. В каждой не хватает нужных переменных, которые во второй,
+#  и если разделять на разные функции то цикл не срабатывает
 
-# TODO Нужно написать две функции -
+#  Нужно написать две функции -
 #  в одной мы реализуем алгоритм валидации строки с выбрасыванием исключения
 #  в другой заводим цикл по файлу, вилидируем строку, ловим исключения, заполняем выходные файлы.
-#  В таком виде просто странно получается - выбростили исключение, и тут же его обработали.
-for line in file:
+# #  В таком виде просто странно получается - выбростили исключение, и тут же его обработали.
+def processing():
+    file_name = 'registrations.txt'
+    file = open(file_name, mode='r')
+    for line in file:
+        def check():
+            name, mail, age = line.split()
+            if name.isalpha() is False:
+                raise NamesError(f"имя неправильно - {line}")
+            elif '@' not in mail or '.' not in mail:
+                raise MailError(f"почта неправильна - {line}")
+            elif int(age) > 99 or int(age) < 10:
+                raise AgeError(f"возраст неправильный - {line}")
+            else:
+                file_name = 'registrations_good.probe.log'
+                file = open(file_name, mode='a')
+                file_content = line
+                file.write(file_content)
+                file.close()
 
-    try:
-        # TODO Можно сразу
-        #  name, mail, age = line.split()
-        linelist = line.split()
-        name = linelist[0]
-        mail = linelist[1]
-        age = linelist[2]
-
-        if name.isalpha() is False:
-            raise nameError
-        elif '@' not in mail or '.' not in mail:
-            raise mailError
-        elif int(age) > 99 or int(age) < 10:
-            raise ageError
-
-
-        else:
-            file_name = 'registrations_good.probe.log'
+        try:
+            check()
+        except (IndexError, NamesError, AgeError, MailError, ValueError) as error:
+            file_name = 'registrations_bad.probe.log'
             file = open(file_name, mode='a')
-            file_content = line
+            file_content = str(error) + ' '
             file.write(file_content)
             file.close()
-
-    # TODO Можно сообщение ошибки формировать на этапе выброса этой ошибки -
-    #   raise NameError(f"имя неправильно - {line}")
-    #   (для IndexError сам интерпретатор сообщение сформирует)
-    #   Тогда обработка всех исключений станет одинаковой и можно будет сделать ее в одном except -
-    #   except (IndexError, NameError, ...) as error:
-    #           ...
-    except IndexError as first_error:
-        file_name = 'registrations_bad.probe.log'
-        file = open(file_name, mode='a')
-        file_content = str('Отсутствует элемент - ') + line
-        file.write(file_content)
-        file.close()
-    except nameError as first_error:
-        file_name = 'registrations_bad.probe.log'
-        file = open(file_name, mode='a')
-        file_content = str('имя неправильно - ') + line
-        file.write(file_content)
-        file.close()
-    except mailError as second_error:
-        file_name = 'registrations_bad.probe.log'
-        file = open(file_name, mode='a')
-        file_content = str('почта неверна - ') + line
-        file.write(file_content)
-        file.close()
-    except ageError as third_error:
-        file_name = 'registrations_bad.probe.log'
-        file = open(file_name, mode='a')
-        file_content = str('возраст неверен - ') + line
-        file.write(file_content)
-        file.close()
-
-file.close()
