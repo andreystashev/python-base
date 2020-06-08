6e2490f74239b143e83fc226541a8a030b1762f7
andrey <>
2020-06-08 22:28:58 +0300

    модуль 9 1 задание

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -27,6 +27,8 @@ import zipfile
 class Counter:
     prev_char = 'A'
     char_summa = 0
+    letters_list = []
+    numbers_list = []
 
     def __init__(self, zip_file_name):
         self.zip_file_name = zip_file_name
@@ -55,118 +57,82 @@ class Counter:
             print('+{txt:-^30}+'.format(txt='+'))
 
     def statistic(self):
-        self.sort()
+        for _, count in self.stat.items():
+            for letters, numbers in count.items():
+                self.letters_list.append(letters)
+                self.sort()
+                self.numbers_list.append(numbers)
+                for char_counts in self.numbers_list:
+                    self.char_summa += char_counts
+            for char in self.letters_list:
+                count.get(char)
+                print('|{txt:^14}|'.format(txt=char), '{txt:^14}|'.format(txt=count.get(char)))
         print('+{txt:-^30}+'.format(txt='+'))
         print('|{txt:^14}|'.format(txt='итого'), '{txt:^14}|'.format(txt=self.char_summa))
         print('+{txt:-^30}+'.format(txt='+'))
 
-    def sort(self):
-        # for _, count in self.stat.items():
-        #     letters_list = []
-        #     numbers_list = []
-        #     for letters, numbers in count.items():
-        #         letters_list.append(letters)
-        #         letters_list.sort()
-        #         numbers_list.append(numbers)
-        #         for char_counts in numbers_list:
-        #             self.char_summa += char_counts
-        #     for char in letters_list:
-        #         count.get(char)
-        #         print('|{txt:^14}|'.format(txt=char), '{txt:^14}|'.format(txt=count.get(char)))
-        pass
-# Todo если раскомментировать sort из основного класса, то код работает. Но при попытке его сделать в методе другого класса
-#  он никак не хочет срабатывать. Также при попытке разделить код из sort он не хочет работать, не могу выделить одну строчку
-#  с которой можно было бы работать в наследниках
-
-# TODO: а как вы это делаете? Покажите код, разберемся
-
-class LettersPlus(Counter):
-    def __init__(self):
-        super().__init__(self)
-
-    def sort(self):
+    def count_statistic(self):
         for _, count in self.stat.items():
-            letters_list = []
-            numbers_list = []
-            for letters, numbers in count.items():
-                letters_list.append(letters)
-                letters_list.sort()
-                numbers_list.append(numbers)
-                for char_counts in numbers_list:
-                    self.char_summa += char_counts
-            for char in letters_list:
-                count.get(char)
-                print('|{txt:^14}|'.format(txt=char), '{txt:^14}|'.format(txt=count.get(char)))
-
-
-class LettersMin(Counter):
-    def sort(self):
-        for _, count in self.stat.items():
-            letters_list = []
-            numbers_list = []
-            for letters, numbers in count.items():
-                letters_list.append(letters)
-                letters_list.sort(reverse=True)
-                numbers_list.append(numbers)
-                for char_counts in numbers_list:
-                    self.char_summa += char_counts
-            for char in letters_list:
-                count.get(char)
-                print('|{txt:^14}|'.format(txt=char), '{txt:^14}|'.format(txt=count.get(char)))
-
-
-class CountPlus(Counter):
-    def sort(self):
-        for _, count in self.stat.items():
-            letters_list = []
-            numbers_list = []
             count = dict(zip(count.values(), count.keys()))
             for letters, numbers in count.items():
-                letters_list.append(letters)
-                letters_list.sort()
-                numbers_list.append(letters)
-                for char_counts in numbers_list:
+                self.letters_list.append(letters)
+                self.sort()
+                self.numbers_list.append(letters)
+                for char_counts in self.numbers_list:
                     self.char_summa += char_counts
-            for char in letters_list:
+            for char in self.letters_list:
                 count.get(char)
                 print('|{txt:^14}|'.format(txt=count.get(char)), '{txt:^14}|'.format(txt=char))
+        print('+{txt:-^30}+'.format(txt='+'))
+        print('|{txt:^14}|'.format(txt='итого'), '{txt:^14}|'.format(txt=self.char_summa))
+        print('+{txt:-^30}+'.format(txt='+'))
 
 
-class CountMin(Counter):
+class Countmin(Counter):
     def sort(self):
-        for _, count in self.stat.items():
-            letters_list = []
-            numbers_list = []
-            count = dict(zip(count.values(), count.keys()))
-            for letters, y in count.items():
-                letters_list.append(letters)
-                letters_list.sort(reverse=True)
-                numbers_list.append(letters)
-                for char_counts in numbers_list:
-                    self.char_summa += char_counts
-            for char in letters_list:
-                count.get(char)
-                print('|{txt:^14}|'.format(txt=count.get(char)), '{txt:^14}|'.format(txt=char))
+        self.letters_list.sort()
 
 
-counter = Counter(
-    zip_file_name='/Users/andrey/PycharmProjects/python_base/lesson_009/python_snippets/voyna-i-mir.txt.zip'
-)
-counter.unzip()
-counter.initialise(file_name='voyna-i-mir.txt')
-l_plus = LettersPlus()
-l_plus.sort()
-counter.statistic()
+class Countmax(Counter):
+    def sort(self):
+        self.letters_list.sort(reverse=True)
+
+
+class Lettersmin(Counter):
+    def sort(self):
+        self.letters_list.sort()
+
+
+class Lettersmax(Counter):
+    def sort(self):
+        self.letters_list.sort(reverse=True)
+
+
+# sort_method = Countmin(zip_file_name='/Users/andrey/PycharmProjects/python_base/lesson_009/python_snippets/voyna-i-mir.txt.zip')
+# sort_method.unzip()
+# sort_method.initialise(file_name='voyna-i-mir.txt')
+# sort_method.count_statistic()
+
+# sort_method = Countmax(zip_file_name='/Users/andrey/PycharmProjects/python_base/lesson_009/python_snippets/voyna-i-mir.txt.zip')
+# sort_method.unzip()
+# sort_method.initialise(file_name='voyna-i-mir.txt')
+# sort_method.count_statistic()
+
+
+# sort_method = Lettersmin(zip_file_name='/Users/andrey/PycharmProjects/python_base/lesson_009/python_snippets/voyna-i-mir.txt.zip')
+# sort_method.unzip()
+# sort_method.initialise(file_name='voyna-i-mir.txt')
+# sort_method.statistic()
+
+sort_method = Lettersmax(
+    zip_file_name='/Users/andrey/PycharmProjects/python_base/lesson_009/python_snippets/voyna-i-mir.txt.zip')
+sort_method.unzip()
+sort_method.initialise(file_name='voyna-i-mir.txt')
+sort_method.statistic()
 
 #  Хорошо, теперь применим паттерн "Шаблонный метод"
 #  То есть в базовом классе оставить метод для получения отсортированной статистики пустым,
 #  а потом сделать его наследников, в которых переопределить этот метод соответствующим образом
-#  не могу понять, как правильно сделать, чтобы наследники начали работать. В задании 8 модуля с семьей
-#  похожий код работал, а тут не хватает какого-то кода. Пробовал через супер и деф __инит__ добавить, но
-#  не дает результата. Пишет, что не хватает селф, но я не пойму почему здесь его требует а в задании с семьей
-#  к примеру этого не было. И само это взаимодействие не понятно, в примере шаблона не разобрался, в интренете
-#  тоже примеры описаны так что не могу уловить суть. Тоесть зачем метод, что он делает, и как он должен выглядеть
-#  относительно понятно, а реализовать в коде не получается
 
 # Смотрите, у вас во всех наследниках код по сути одинаковый, отличается только сортировка статистики.
 #  То есть у вас есть базовый класс, в котором определены методы -
