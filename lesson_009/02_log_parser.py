087e42bf639004e7ba174150bd369925cf7f63a7
andrey <>
2020-06-08 20:59:48 +0300

    модуль 9 2 задание

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -28,95 +28,78 @@ class Parser:
         self.file_name = file_name
 
     def time_cut(self):
-        x = 11
-        return x
-        # pass
+        pass
 
     def calculation(self):
         count = 0
-        x = parser.time_cut()  # TODO: этот метод вызываем у self
-
-        # todo получилось сделать корректное высчитывание, но моим способом не получается корректно высчитать год и месяц, потому
-        #  что срез prev_date до и после циклов становится одинаковым, приходится брать информацию из файла
-        # TODO: не очень понял комментарий.
-
-        prev_date = str(' 2018-05-14 19:38'[1:x])  # TODO: значения из самого файла не должны фигурировать в коде.
-                                                   # TODO: Просто берите значение из нулевой строчки файла на нулевой итерации
+        x = self.time_cut()
         with open(self.file_name, mode='r') as file:
+            prev_date = (next(file)[1:x])
+
             for line in file:
                 if 'NOK' in line:
                     if prev_date in line:
                         count += 1
-                        if count == 4968:  # TODO: что это за магическое число?
-                            self.interim_date_list.append((prev_date, count))
-                    elif prev_date not in line:  # TODO: можно не уточнять после elif, выбора особо-то и нет
+                        self.interim_date_list.append((prev_date, count))
+                        self.date_dict.update(self.interim_date_list)
+                    else:
                         self.interim_date_list.append((prev_date, count))
                         prev_date = str(line[1:x])
                         count = 1
-                    for _ in self.interim_date_list:
-                        self.date_dict.update(self.interim_date_list)  # TODO: зачем это делать в цикле?
-                        break
+                        self.date_dict.update(self.interim_date_list)
 
     def create_file(self, file_name):
         file_name = file_name
+        file = open(file_name, mode='a')
         for time, nok_number in self.date_dict.items():
-            file = open(file_name, mode='a')  # TODO: не нужно открывать и закрывать файл на каждой итерации
-            file_content = ('[' + str(time) + '] ' + str(nok_number) + '\n')  # TODO: используйте f-строки, либо .format() для составления строкового вывода
-            file.write(file_content)
-            file.close()
+            if nok_number > 0:
+                file_content = ('[{}] {}\n'.format(str(time), str(nok_number)))
+                file.write(file_content)
+        file.close()
+
+
+class Year(Parser):
+    def time_cut(self):
+        x = 5
+        return x
+
+
+class Month(Parser):
+    def time_cut(self):
+        x = 8
+        return x
 
 
 class Hour(Parser):
     def time_cut(self):
-        x = 17  # TODO: на самом деле тут кончаются минуты
+        x = 14
         return x
 
 
-parser = Parser(file_name='events.txt')
-parser.calculation()
-parser.create_file(file_name='events.nok.txt')
-hr = Hour(Parser)  # TODO: Напишите, что здесь должно произойти?
-hr.time_cut()
+class Minute(Parser):
+    def time_cut(self):
+        x = 17
+        return x
+
+
+minute = Minute(file_name='events.txt')
+minute.calculation()
+minute.create_file(file_name='events.nok.txt')
+
+# hour = Hour(file_name='events.txt')
+# hour.calculation()
+# hour.create_file(file_name='events.nok.txt')
+
+# month = Month(file_name='events.txt')
+# month.calculation()
+# month.create_file(file_name='events.nok.txt')
+
+# year = Year(file_name='events.txt')
+# year.calculation()
+# year.create_file(file_name='events.nok.txt')
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
 #  - по году
 # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
-
-# не пойму, почему не так считает, пробовал разные команды:
-
-# z = dict.fromkeys([date],value)
-# minute_d1.update(z)
-# dd = collections.defaultdict(None,([date],value))
-# print(dd)
-# if date in minute_d1.keys():
-#     value += 1
-#     print('ge')
-# #     # minute_d1.update(z)
-# else:
-#     minute_d1.update(date,value)
-#     print(minute_d1)
-# minute_d1.values(value)
-# print(minute_d1)
-#  я так понял, что правильным решением будет не создавать лишний словарь и список, а изначально один словарь
-#  расширять потому что либо создается один словарь к каждой строке, либо туда за раз все ключи со значениями
-#  добавляются и повторяющиеся исчезают, поэтому после этого не работает проверка на  наличие ключа
-#  Здесь можно вообще без словаря обойтись, подскзал в самом классе
-
-
-# Неверно статистику по минутам выводит. Можно увидеть из исхоного файла, что начитаться выходной файл должен так -
-# [2018-05-14 19:38] 4
-# [2018-05-14 19:39] 1
-# [2018-05-14 19:40] 1
-# [2018-05-14 19:42] 1
-# [2018-05-14 19:43] 2
-# [2018-05-14 19:44] 1
-# [2018-05-14 19:45] 2
-#  ...
-
-#  по шаблонному методу в предыдущем задании написал, тоже не пойму как сделать
-# И аналогично, применим Шаблонынй метод для получения аналогичного файла за час, месяц, год.
-#  Здесь в наследниках нам достаточно будет только переопределять значение, до которого мы обрезаем строку -
-#   для минут это было 17, для часов будет 14 и т.д.
-# todo по наследникам в первом задании задал вопрос
