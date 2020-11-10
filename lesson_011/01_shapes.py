4355e13ffd0a7dec54bcf01649207d4af3dd99d0
andrey <88stashew@icloud.com>
2020-11-10 21:41:29 +0300

    11.1

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -13,22 +13,21 @@ import simple_draw as sd
 #
 # Функция-фабрика должна принимать параметр n - количество сторон.
 
-
-# TODO давайте доработаем функцию видно что на 8 угольнике крайний вектор не доходит,
-# TODO чтобы это исправить нужно использовать sd.line() после цикла for
 def get_polygon(n):
     def any_shape(point, length, angle):
-        # TODO эту переменную вынести в функцию get_polygon в ее область видимости!
+        startpoint = point
+        # TODO нижнюю строчку нужно переместить в цикл фор?
         angle_change = 360 // n
-        # TODO что есть переменная v и где она используется ? Угол наклона вектора получать тут
-        # TODO Что бы не "крутить" крайний вектор нужно в рендж сделать так (0, 360-angle_change, angle_change):
-        for v in range(0, 360, angle_change):
-            v = sd.get_vector(start_point=point, angle=angle, length=length)
-            v.draw()
-            point = v.end_point
-            # TODO Угол наклона вектора получать в цикле!
+        #  что есть переменная v и где она используется ? Угол наклона вектора получать тут
+        for _ in range(0, 360 - angle_change, angle_change):
+            side = sd.get_vector(start_point=point, angle=angle, length=length)
+            side.draw()
+            point = side.end_point
+            # Угол наклона вектора получать в цикле!
+            # TODO нижнюю строчку нужно переместить отсюда? Если производить какие-то манипуляции со строчкаим, которые
+            #  тудушками отмечены, то тогда код ломается
             angle += angle_change
-        # TODO тут рисовать линию от начало до point, начальную точку нужно сохранить изначально!
+        sd.line(start_point=startpoint, end_point=side.end_point)
 
     return any_shape(point=sd.get_point(200, 200), length=200, angle=0)
 
