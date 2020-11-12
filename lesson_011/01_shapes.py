83aa1bfb9b24d0547fad345ea11629534f334257
andrey <88stashew@icloud.com>
2020-11-12 23:18:49 +0300

    11.1

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -16,22 +16,15 @@ import simple_draw as sd
 def get_polygon(n):
     angle_change = 360 // n
 
-    # TODO тут назовите angle -> zero_angel
-    def any_shape(point, length, angle):
-        # TODO нейминг переменной
-        startpoint = point
-        # TODO в цикле мы будем получать angle
-        for _ in range(0, 360 - angle_change, angle_change):
-            # TODO в sd.get_vector в параметре angle напишите сразу angle=angle+zero_angel
-            side = sd.get_vector(start_point=point, angle=angle, length=length)
+    def any_shape(point, length, zero_angle):
+        start_point = point
+        for angle in range(0, 360 - angle_change, angle_change):
+            side = sd.get_vector(start_point=point, angle=angle+zero_angle, length=length)
             side.draw()
             point = side.end_point
-            # TODO Это строка нам не нужна если мы будем получать angle в цикле
-            angle += angle_change
-        # TODO используйте переменные start_point и point
-        sd.line(start_point=startpoint, end_point=side.end_point)
+        sd.line(start_point=start_point, end_point=point)
 
-    return any_shape(point=sd.get_point(200, 200), length=200, angle=0)
+    return any_shape(point=sd.get_point(200, 200), length=200, zero_angle=0)
 
 
 get_polygon(n=8)
