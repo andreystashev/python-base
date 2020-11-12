245d84a3e190d6e026e8dc6d1d6b2f16069d8949
user74 <housechel@mail.ru>
2020-11-12 11:51:53 +0500

    Lesson 011 01 - Corrected

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -14,19 +14,21 @@ import simple_draw as sd
 # Функция-фабрика должна принимать параметр n - количество сторон.
 
 def get_polygon(n):
+    angle_change = 360 // n
+
+    # TODO тут назовите angle -> zero_angel
     def any_shape(point, length, angle):
+        # TODO нейминг переменной
         startpoint = point
-        # TODO нижнюю строчку нужно переместить в цикл фор?
-        angle_change = 360 // n
-        #  что есть переменная v и где она используется ? Угол наклона вектора получать тут
+        # TODO в цикле мы будем получать angle
         for _ in range(0, 360 - angle_change, angle_change):
+            # TODO в sd.get_vector в параметре angle напишите сразу angle=angle+zero_angel
             side = sd.get_vector(start_point=point, angle=angle, length=length)
             side.draw()
             point = side.end_point
-            # Угол наклона вектора получать в цикле!
-            # TODO нижнюю строчку нужно переместить отсюда? Если производить какие-то манипуляции со строчкаим, которые
-            #  тудушками отмечены, то тогда код ломается
+            # TODO Это строка нам не нужна если мы будем получать angle в цикле
             angle += angle_change
+        # TODO используйте переменные start_point и point
         sd.line(start_point=startpoint, end_point=side.end_point)
 
     return any_shape(point=sd.get_point(200, 200), length=200, angle=0)
