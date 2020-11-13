55866803d028b8427a53d7a3bbc20f67403c56f7
user74 <housechel@mail.ru>
2020-11-13 15:01:40 +0500

    Lesson 011 04 - Completed

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -16,7 +16,7 @@ def log_errors(func):
             file_content1 = ('<{}>  <{}>  <{}>  <{}>\n'.format(func.__name__, kwargs, Exception, z_error))
             with open('function_errors.log', 'a', encoding='utf8') as file:
                 file.write(file_content1)
-                raise ZeroDivisionError
+            raise ZeroDivisionError
 
         except ValueError as v_error:
             file_content2 = ('<{}>  <{}>  <{}>  <{}>\n'.format(func.__name__, args, Exception, v_error))
@@ -66,3 +66,4 @@ perky(param=42)
 # def func():
 #     pass
 
+# зачет!
