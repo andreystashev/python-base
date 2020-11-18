0a8d22e8e6a5af956355f6bb2d449afe624dc4d5
andrey <88stashew@icloud.com>
2020-11-18 13:15:57 +0300

    13.1

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+from PIL import Image, ImageDraw, ImageFont, ImageColor
 
 
 # Заполнить все поля в билете на самолет.
@@ -8,9 +10,34 @@
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
 
+
 def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+    template = 'images/ticket_template.png'
+    font_path = os.path.join('fonts', 'ofont.ru_Frusta.ttf')
+    im = Image.open(template)
+    w, h = im.size
+    im = im.resize((w, h))
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype(font_path, size=18)
+    y = im.size[1] - 220 - (10 + font.size) * 2
+    message = f'{fio}'
+    draw.text((50, y), message, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 188 - font.size
+    message = f'{from_}'
+    draw.text((50, y), message, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 122 - font.size
+    message = f'{to}'
+    draw.text((50, y), message, font=font, fill=ImageColor.colormap['black'])
+    y = im.size[1] - 125 - font.size
+    message = f'{date}'
+    draw.text((285, y), message, font=font, fill=ImageColor.colormap['black'])
+    out_path = 'final_ticket.png'
+    im.save(out_path)
+    print(f'Post card saved az {out_path}')
+
+
+make_ticket(fio='stashev', from_='earth', to='moon', date='12.09')
+
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
