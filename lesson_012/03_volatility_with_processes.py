397d99adddbfaa71014fac5455b4f40d95a4b6b2
andrey <88stashew@icloud.com>
2020-11-30 23:01:40 +0300

    бот

--- lesson_012/03_volatility_with_processes.py
+++ lesson_012/03_volatility_with_processes.py
@@ -17,6 +17,74 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/02_volatility_with_threads.py !!!
+from utilites import time_track, show_result, generate_filenames
 
-# TODO тут ваш код в многопроцессном стиле
+import multiprocessing
+
+
+class Ticker(multiprocessing.Process):
+
+    def __init__(self, ticket_folder, collector=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.ticket_folder = ticket_folder
+        self.name_ticket = ''
+        self.volatility = 0
+        self.collector = collector
+
+    def run(self):
+        self.calculate(self.open())
+        collector = multiprocessing.Queue()
+        # self.collector.put(open())# todo не пойму где нужно применить метод пут
+
+    def open(self):
+        price_scope = []
+        with open(self.ticket_folder, mode='r') as open_ticker:
+            for element in open_ticker:
+                scattered_element = element.split(',')
+                self.name_ticket = scattered_element[0]
+                if scattered_element[2] != 'PRICE':
+                    price_scope.append(float(scattered_element[2]))
+            return price_scope
+
+    def calculate(self, unsorted):
+        unsorted.sort()
+        half_sum = (unsorted[0] + unsorted[-1]) / 2
+        self.volatility = ((unsorted[-1] - unsorted[0]) / half_sum) * 100
+
+
+@time_track
+def main(folder):
+    collector = multiprocessing.Queue()
+    zero_tickers = []
+    value_key = {}
+    sorted_place = []
+    tickers = []
+
+    for last_folder in generate_filenames(folder):
+        tickers.append(Ticker(last_folder))
+    while not collector.empty(): # todo такая конструкция из обучения не сработает для отлова пустоты?
+        for ticker in tickers:
+            ticker.start()
+        for ticker in tickers:
+            ticker.join()
+
+
+
+
+    for ran_ticker in tickers:
+        if ran_ticker.volatility == 0:
+            zero_tickers.append(ran_ticker.name_ticket)
+        else:
+            value_key[ran_ticker.volatility] = ran_ticker.name_ticket
+            sorted_place.append(ran_ticker.volatility)
+            sorted_place.sort()
+
+    show_result(sorted_place, value_key, zero_tickers)
+
+
+# Core 4 по 1.4Hz - Функция работала   секунд(ы)
+# Core 4 по 2.4Hz - Функция работала   секунд(ы)
+path = "trades/"
+if __name__ == '__main__':
+    main(folder=path)
